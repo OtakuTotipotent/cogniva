e98029d702f83db2e5130066ca4c5854d7a96181
OtakuTotipotent <otakutotipotent@gmail.com>
2026-06-18 19:09:04 +0500

    feat: add chat model & chat create route

---
+++ models/Chat.js
@@ -0,0 +1,22 @@
+import mongoose, { mongo } from "mongoose";
+
+const ChatSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    messages: [
+      {
+        role: { type: String, required: true },
+        content: { type: String, required: true },
+        timestamp: { type: String, required: true },
+      },
+    ],
+    userId: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Chat = mongoose.models.Chat || mongoose.model("Chat", ChatSchema);
+
+export default Chat;
